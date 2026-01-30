3b93149b9f8776e0dd11a733c432e272540c67b3
g ve <gloria.laura.vela@gmail.com>
2026-01-30 16:01:11 -0500

    test postman

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ const PORT = 5000;
 app.use(express.json()); // the express app uses the middleware express.json() to handle the request as a json object.
 app.use("/user", routes); //the express app uses routes to handle the endpoints which start with /user. This means that for all the endpoints starting with /user, the server will go and look for an endpoint handler in users.js.
 
+
 // Start the server and log a message when it's running
 app.listen(PORT, () => console.log("Server is running at port " + PORT));
